d84e01fbb5066b4153efe4493ff6a9a14c8b1167
Josh Ellis <joshe523@gmail.com>
2020-04-13 17:32:02 -0500

    Redirect from login to home when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to "/" unless session[:user_id].nil?
   end
 
   def create
     user = User.find_by(email: params[:user][:email])
     if user&.authenticate(params[:user][:password])
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else
       redirect_to :new
