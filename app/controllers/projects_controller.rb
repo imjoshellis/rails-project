398ff1a1f09d23448d447b6e545e0da2db5ea981
Josh Ellis <joshe523@gmail.com>
2020-04-13 17:12:41 -0500

    Setup require login on projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,12 @@
 class ProjectsController < ApplicationController
+  before_action :require_login
+
+  def index
+  end
+
+  private
+
+  def require_login
+    redirect_to "/login" unless session.include? :user_id
+  end
 end
