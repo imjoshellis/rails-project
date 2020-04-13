d41c1978dd90085d633ab7d1e439e1a0983a2697
Josh Ellis <joshe523@gmail.com>
2020-04-13 17:35:51 -0500

    Add logout route and method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    redirect_to "/" unless session[:user_id].nil?
+    redirect_to root_path unless session[:user_id].nil?
   end
 
   def create
     user = User.find_by(email: params[:user][:email])
     if user&.authenticate(params[:user][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to root_path
     else
-      redirect_to :new
+      redirect_to login_path
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
