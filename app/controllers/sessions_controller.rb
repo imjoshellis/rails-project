834ad76b95a4e6a040533942f238e9aa08c88863
Josh Ellis <joshe523@gmail.com>
2020-04-13 17:23:09 -0500

    Setup login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:user][:email])
+    if user&.authenticate(params[:user][:password])
+      redirect_to user_path(user)
+    else
+      redirect_to :new
+    end
   end
 end
