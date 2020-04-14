061a0175e89f4c8cd989b2ca49cccd4c8aeb90a2
Josh Ellis <joshe523@gmail.com>
2020-04-14 16:14:27 -0500

    Add github oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   belongs_to :role, default: -> { Role.find_by(name: "developer") }
   has_many :user_projects
   has_many :projects, through: :user_projects
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |u|
+      u.provider = auth.provider
+      u.name = auth.info.name
+      u.uid = auth.uid
+      u.email = auth.info.email
+      u.password = Devise.friendly_token[0,20]
+    end
+  end
 end
