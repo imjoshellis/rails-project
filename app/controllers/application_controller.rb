48251f9e73baf3f1727a1ad9dec8492c0dcb0574
Josh Ellis <joshe523@gmail.com>
2020-04-14 10:32:35 -0500

    Add #current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def require_login
     redirect_to "/login" unless session.include? :user_id
   end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
 end
