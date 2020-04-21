0bcc0f89395dee8ce5ee61d1000b4dc77bad148e
Josh Ellis <joshe523@gmail.com>
2020-04-21 12:58:29 -0500

    Fix validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,6 +4,6 @@ class Story < ApplicationRecord
 
   validates :name, presence: true, length: {in: 6..20}
   validates :description, length: {maximum: 600}
-  validates :status, inclusion: {in: %w[open doing closed], message: "%{value} is not a valid status"}
+  validates :status, inclusion: {in: %w[todo doing closed], message: "%{value} is not a valid status"}
   validates :effort, :value, inclusion: 1..10
 end
