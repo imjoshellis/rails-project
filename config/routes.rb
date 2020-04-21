ccb043471187ee7fa1bc2f86c323e7b1cce3a9a8
Josh Ellis <joshe523@gmail.com>
2020-04-21 13:02:02 -0500

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   resources :roles
   resources :users
   resources :stories
-  resources :sprints
+  resources :sprints do
+    resources :stories
+  end
   resources :goals
-  resources :projects
+  resources :projects do
+    resources :goals
+    resources :sprints
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
