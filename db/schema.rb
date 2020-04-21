1a181651bf635fc7e62e69db200799e24ed50f96
Josh Ellis <joshe523@gmail.com>
2020-04-21 13:02:18 -0500

    Format schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_14_204159) do
-
   create_table "goals", force: :cascade do |t|
     t.integer "project_id"
     t.integer "sprint_id"
@@ -77,5 +76,4 @@ ActiveRecord::Schema.define(version: 2020_04_14_204159) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
