abc78360408806b2d91dd7b688f40b0673740150
Josh Ellis <joshe523@gmail.com>
2020-04-21 12:59:58 -0500

    Add index

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,4 +1,11 @@
 class SprintsController < ApplicationController
+  def index
+    @sprints = if params[:project_id]
+      Project.find(params[:project_id]).sprints
+    else
+      Sprint.all
+    end
+  end
 
   def show
     @sprint = Sprint.find(params[:id])
