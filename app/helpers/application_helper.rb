9a4b217e69e7dfb7fb9bad23e088eefbd005f14f
Josh Ellis <joshe523@gmail.com>
2020-06-22 10:09:32 -0500

    Style project menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def menu_link_class(this_link, current)
-    this_link == current ? "is-active" : ""
+    this_link == current ? "is-active font-bold" : ""
+  end
+
+  def menu_project_name(this_link, current, project_name)
+    this_link == current ? "📂 " + project_name : "📁 " + project_name
   end
 end
