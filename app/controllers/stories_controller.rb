e1f4b551eeb4ebf0f02e22acfc56b31f40fcd69d
Josh Ellis <joshe523@gmail.com>
2020-06-23 10:12:50 -0500

    Story view setup

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,16 @@
 class StoriesController < ApplicationController
+  def index
+    @stories = if params[:sprint_id]
+      Sprint.find(params[:sprint_id]).stories
+    else
+      Story.all
+    end
+  end
+
   def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
   end
 end
