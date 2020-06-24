0a3a08f761c07f3b21f94b9d144cb20f9b742e82
Josh Ellis <joshe523@gmail.com>
2020-06-24 11:26:23 -0500

    Setup sprint new form

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -12,5 +12,17 @@ class SprintsController < ApplicationController
   end
 
   def new
+    @sprint = Sprint.new
+  end
+
+  def create
+    @sprint = Sprint.new
+    @project = Project.find_by(name: params[:sprint][:project])
+    @sprint.update(name: params[:sprint][:name], project_id: @project.id)
+    if @sprint.errors.any?
+      render :new
+    else
+      redirect_to project_sprint_path(@project, @sprint)
+    end
   end
 end
