4adc4fe29880eecb23580bfb4c165348b09b9fce
Josh Ellis <joshe523@gmail.com>
2020-06-26 12:05:08 -0500

    Add create and destroy goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -32,4 +32,27 @@ class GoalsController < ApplicationController
       redirect_to project_goal_path(@project, @goal)
     end
   end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new
+    @sprint = Sprint.find(params[:goal][:sprint_id])
+    @project = @sprint.project
+    @goal.update(name: params[:goal][:name], project_id: @project.id, sprint_id: @sprint.id, description: params[:goal][:description])
+    if @goal.errors.any?
+      render :new
+    else
+      redirect_to project_goal_path(@project, @goal)
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @project = @goal.project
+    @goal.destroy
+    redirect_to project_path(@project)
+  end
 end
