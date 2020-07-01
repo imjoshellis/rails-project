a2c7ee64db3edd9ff1d914cc9de1f16763db4949
Josh Ellis <joshe523@gmail.com>
2020-07-01 15:48:42 -0500

    Add validation for sprint in goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,8 +4,16 @@ class Goal < ApplicationRecord
 
   validates :name, presence: true, length: {in: 6..40}
   validates :description, length: {maximum: 600}
+  validate :match_project_goals
 
   def project_name
     project.name
+  end
+
+  private
+
+  def match_project_goals
+    return unless sprint
+    errors.add(:sprint, "must match project") unless project.sprints.include?(sprints)
   end
 end
